3e12d6c13069c241a05ec0f4f2ac88515da1bbdf
James <james@plasko.com>
2017-03-17 22:59:36 -0700

    Yay

--- decode.py
+++ decode.py
@@ -10,20 +10,7 @@ def main():
       print("Unable to open {:s}".format(sys.argv[1]))
       sys.exit(1) 
 
-#   loss = []
    new_file = open("decode.ppm","w")
-
-#   for val in the_file:
-#      okay = val.split(" ")
-#      for value in okay:
-#         loss.append(value.rstrip())
-
-#   new_file.write("{:s}\n".format(loss[0]))
-#   new_file.write("{:s} {:s}\n".format(loss[1],loss[2]))
-#   new_file.write("{:s}\n".format(loss[3]))
-
-#   loss = groups_of_3(loss)
-
    counter = 0
    pixel = []
 
@@ -43,31 +30,6 @@ def main():
                new_file.write("{:d} {:d} {:d}\n".format(red,green,blue))
                pixel = []
       counter += 1
-
-#   for pix in loss:
-#      if counter != 0:
-#         red = int(pix[0])*10
-#         if red > 255:
-#            red = 255
-#         blue = red
-#         green = red
-#         new_file.write("{:d} {:d} {:d}\n".format(red,green,blue))
-#      counter += 1
-
-def groups_of_3(values):
-   inside_list = []
-   new_list = []
-   count = 0
-   for val in range(4,len(values)):
-      inside_list.append(values[val])
-      count += 1
-      if val == len(values) - 1:
-         new_list.append(inside_list)
-         return new_list
-      elif count == 3:
-         new_list.append(inside_list)
-         inside_list = []
-         count = 0
 
 
 if __name__ == '__main__':
--- fade.py
+++ fade.py
@@ -14,30 +14,59 @@ def main():
       print("Unable to open {:s}".format(sys.argv[1]))
       sys.exit(1)
 
-   loss = []
+#   loss = []
    new_file = open("fade.ppm","w")
 
-   for val in the_file:
-      okay = val.split(" ")
-      for value in okay:
-         loss.append(value.rstrip())
+#   for val in the_file:
+#      okay = val.split(" ")
+#      for value in okay:
+#         loss.append(value.rstrip())
 
-   new_file.write("{:s}\n".format(loss[0]))
-   new_file.write("{:s} {:s}\n".format(loss[1],loss[2]))
-   new_file.write("{:s}\n".format(loss[3]))
+#   new_file.write("{:s}\n".format(loss[0]))
+#   new_file.write("{:s} {:s}\n".format(loss[1],loss[2]))
+#   new_file.write("{:s}\n".format(loss[3]))
 
-   loss_2 = groups_of_3(loss)
+#   loss_2 = groups_of_3(loss)
 
-   for okay in range(len(loss_2)):
-      row = okay//int(loss[1])
-      column = okay%int(loss[1])
-      scale = (r - (math.sqrt(abs(y-row)**2 + abs(x-column)**2))) / r
-      if scale < .2:
-         scale = .2
-      red = int(loss_2[okay][0]) * scale
-      green = int(loss_2[okay][1]) * scale
-      blue = int(loss_2[okay][2]) * scale
-      new_file.write("{:d} {:d} {:d}\n".format(int(red),int(green),int(blue)))
+   counter = 0
+   counter_2 = 0
+   pixel = []
+
+   for line in the_file:
+      if counter < 3:
+         new_file.write("{:s}".format(line))
+         if counter == 1:
+            k = line.split(" ")
+            amount = int(k[0])*int(k[1])
+            width = k[0]
+      else:
+         val = line.split(" ")
+         for num in val:
+            pixel.append(num)
+            if len(pixel) == 3:
+               row = counter_2//int(width)
+               column = counter_2%int(width)
+               scale = (r - (math.sqrt(abs(y-row)**2 + abs(x-column)**2))) / r
+               if scale < .2:
+                  scale = .2
+               red = int(pixel[0]) * scale
+               green = int(pixel[1]) * scale
+               blue = int(pixel[2]) * scale
+               new_file.write("{:d} {:d} {:d}\n".format(int(red),int(green),int(blue)))
+               pixel = []
+               counter_2 += 1
+      counter += 1
+
+#   for okay in range(len(loss_2)):
+#      row = okay//int(loss[1])
+#      column = okay%int(loss[1])
+#      scale = (r - (math.sqrt(abs(y-row)**2 + abs(x-column)**2))) / r
+#      if scale < .2:
+#         scale = .2
+#      red = int(loss_2[okay][0]) * scale
+#      green = int(loss_2[okay][1]) * scale
+#      blue = int(loss_2[okay][2]) * scale
+#      new_file.write("{:d} {:d} {:d}\n".format(int(red),int(green),int(blue)))
 
 def groups_of_3(values):
    inside_list = []
